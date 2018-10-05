39d47de34c947f7792c4583bdef6355f0506bc69
Klemens Muthmann <klemens_muthmann@gmx.de>
2018-10-05 21:51:02 +0200

    Add parameter to set the keystore passwords

--- src/main/java/de/cyface/collector/MainVerticle.java
+++ src/main/java/de/cyface/collector/MainVerticle.java
@@ -1,20 +1,16 @@
 /*
  * Copyright 2018 Cyface GmbH
- * 
  * This file is part of the Cyface Data Collector.
- *
- *  The Cyface Data Collector is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *  
- *  The Cyface Data Collector is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with the Cyface Data Collector.  If not, see <http://www.gnu.org/licenses/>.
+ * The Cyface Data Collector is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * The Cyface Data Collector is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with the Cyface Data Collector. If not, see <http://www.gnu.org/licenses/>.
  */
 package de.cyface.collector;
 
@@ -56,134 +52,136 @@ import io.vertx.ext.web.handler.JWTAuthHandler;
  */
 public class MainVerticle extends AbstractVerticle {
 
-	/**
-	 * The <code>Logger</code> used for objects of this class. Configure it by
-	 * changing the settings in
-	 * <code>src/main/resources/vertx-default-jul-logging.properties</codec>.
-	 */
-	private final static Logger LOGGER = LoggerFactory.getLogger(MainVerticle.class);
+    /**
+     * The <code>Logger</code> used for objects of this class. Configure it by
+     * changing the settings in
+     * <code>src/main/resources/vertx-default-jul-logging.properties</codec>.
+     */
+    private final static Logger LOGGER = LoggerFactory.getLogger(MainVerticle.class);
 
-	@Override
-	public void start(final Future<Void> startFuture) throws Exception {
+    @Override
+    public void start(final Future<Void> startFuture) throws Exception {
 
-		prepareEventBus();
+        prepareEventBus();
 
-		deployVerticles();
+        deployVerticles();
 
-		final Router router = setupRoutes();
+        final Router router = setupRoutes();
 
-		startHttpServer(router, startFuture);
-	}
+        startHttpServer(router, startFuture);
+    }
 
-	/**
-	 * Prepares the Vertx event bus during startup of the application.
-	 */
-	private void prepareEventBus() {
-		vertx.eventBus().registerDefaultCodec(Measurement.class, Measurement.getCodec());
-	}
+    /**
+     * Prepares the Vertx event bus during startup of the application.
+     */
+    private void prepareEventBus() {
+        vertx.eventBus().registerDefaultCodec(Measurement.class, Measurement.getCodec());
+    }
 
-	/**
-	 * Deploys all additional <code>Verticle</code> objects required by the
-	 * application.
-	 */
-	private void deployVerticles() {
-		final DeploymentOptions options = new DeploymentOptions().setWorker(true).setConfig(Parameter.MONGO_DATA_DB.jsonValue(vertx, config()));
-		vertx.deployVerticle(SerializationVerticle.class, options);
-	}
+    /**
+     * Deploys all additional <code>Verticle</code> objects required by the
+     * application.
+     */
+    private void deployVerticles() {
+        final DeploymentOptions options = new DeploymentOptions().setWorker(true)
+                .setConfig(Parameter.MONGO_DATA_DB.jsonValue(vertx, config()));
+        vertx.deployVerticle(SerializationVerticle.class, options);
+    }
 
-	/**
-	 * @return The Vertx router used by this project.
-	 */
-	private Router setupRoutes() {
-		final String keystorePath = this.getClass().getResource("/keystore.jceks").getFile();
-		String jwtKeystoreLocation = Parameter.JWT_KEYSTORE.stringValue(vertx, keystorePath);
-		final JWTAuthOptions config = new JWTAuthOptions()
-				.setKeyStore(new KeyStoreOptions().setPath(jwtKeystoreLocation).setPassword("secret"));
+    /**
+     * @return The Vertx router used by this project.
+     */
+    private Router setupRoutes() {
+        final String keystorePath = this.getClass().getResource("/keystore.jceks").getFile();
+        String jwtKeystoreLocation = Parameter.JWT_KEYSTORE.stringValue(vertx, keystorePath);
+        final JWTAuthOptions config = new JWTAuthOptions()
+                .setKeyStore(new KeyStoreOptions().setPath(jwtKeystoreLocation)
+                        .setPassword(Parameter.JWT_KEYSTORE_PASSWORD.stringValue(vertx, "secret")));
 
-		final JWTAuth jwtAuthProvider = JWTAuth.create(vertx, config);
-		final AuthHandler jwtAuthHandler = JWTAuthHandler.create(jwtAuthProvider, "/api/v2/login");
+        final JWTAuth jwtAuthProvider = JWTAuth.create(vertx, config);
+        final AuthHandler jwtAuthHandler = JWTAuthHandler.create(jwtAuthProvider, "/api/v2/login");
 
-		final JsonObject mongoClientConfig = Parameter.MONGO_USER_DB.jsonValue(vertx, config());
+        final JsonObject mongoClientConfig = Parameter.MONGO_USER_DB.jsonValue(vertx, config());
 
-		final MongoClient client = SerializationVerticle.createSharedMongoClient(vertx, mongoClientConfig);
-		final JsonObject authProperties = new JsonObject();
-		final MongoAuth authProvider = MongoAuth.create(client, authProperties);
-		final List<String> roles = new ArrayList<>();
-		final List<String> permissions = new ArrayList<>();
-		// TODO: Remove before going into production Deletes all users and creates admin
-		// account
-		client.removeDocuments("user", new JsonObject(), r -> {
-			authProvider.insertUser("admin", "secret", roles, permissions,
-					ir -> LOGGER.info("Identifier of new user id: " + ir));
-		});
+        final MongoClient client = SerializationVerticle.createSharedMongoClient(vertx, mongoClientConfig);
+        final JsonObject authProperties = new JsonObject();
+        final MongoAuth authProvider = MongoAuth.create(client, authProperties);
+        final List<String> roles = new ArrayList<>();
+        final List<String> permissions = new ArrayList<>();
+        // TODO: Remove before going into production Deletes all users and creates admin
+        // account
+        client.removeDocuments("user", new JsonObject(), r -> {
+            authProvider.insertUser("admin", "secret", roles, permissions,
+                    ir -> LOGGER.info("Identifier of new user id: " + ir));
+        });
 
-		final Router mainRouter = Router.router(vertx);
-		final Router v2ApiRouter = Router.router(vertx);
+        final Router mainRouter = Router.router(vertx);
+        final Router v2ApiRouter = Router.router(vertx);
 
-		v2ApiRouter.route("/login").handler(BodyHandler.create()).handler(ctx -> {
-			try {
-				JsonObject body = ctx.getBodyAsJson();
-				authProvider.authenticate(body, r -> {
-					if (r.succeeded()) {
-						LOGGER.info("Authentication successful!");
-						String generatedToken = jwtAuthProvider.generateToken(body,
-								new JWTOptions().setExpiresInSeconds(60));
-						LOGGER.info("New JWT Token: " + generatedToken);
-						ctx.response().putHeader("Content-Type", "text/plain").setStatusCode(200).end(generatedToken);
-					} else {
-						ctx.response().setStatusCode(401).end();
-					}
-				});
-			} catch (DecodeException e) {
-				LOGGER.error("Unable to decode body!", e);
-				ctx.response().setStatusCode(404).end();
-			}
-		});
-		mainRouter.route().handler(jwtAuthHandler);
-		mainRouter.route().last().handler(new DefaultHandler());
-		mainRouter.mountSubRouter("/api/v2", v2ApiRouter);
+        v2ApiRouter.route("/login").handler(BodyHandler.create()).handler(ctx -> {
+            try {
+                JsonObject body = ctx.getBodyAsJson();
+                authProvider.authenticate(body, r -> {
+                    if (r.succeeded()) {
+                        LOGGER.info("Authentication successful!");
+                        String generatedToken = jwtAuthProvider.generateToken(body,
+                                new JWTOptions().setExpiresInSeconds(60));
+                        LOGGER.info("New JWT Token: " + generatedToken);
+                        ctx.response().putHeader("Content-Type", "text/plain").setStatusCode(200).end(generatedToken);
+                    } else {
+                        ctx.response().setStatusCode(401).end();
+                    }
+                });
+            } catch (DecodeException e) {
+                LOGGER.error("Unable to decode body!", e);
+                ctx.response().setStatusCode(404).end();
+            }
+        });
+        mainRouter.route().handler(jwtAuthHandler);
+        mainRouter.route().last().handler(new DefaultHandler());
+        mainRouter.mountSubRouter("/api/v2", v2ApiRouter);
 
-		v2ApiRouter.post("/measurements").handler(BodyHandler.create().setDeleteUploadedFilesOnEnd(false))
-				.handler(new MeasurementHandler());
-		v2ApiRouter.route().last().handler(new DefaultHandler());
+        v2ApiRouter.post("/measurements").handler(BodyHandler.create().setDeleteUploadedFilesOnEnd(false))
+                .handler(new MeasurementHandler());
+        v2ApiRouter.route().last().handler(new DefaultHandler());
 
-		return mainRouter;
-	}
+        return mainRouter;
+    }
 
-	/**
-	 * Starts the HTTP server provided by this application. This server runs the
-	 * Cyface collector REST-API.
-	 * 
-	 * @param router      The router for all the endpoints the HTTP server should
-	 *                    serve.
-	 * @param startFuture Informs the caller about the successful or failed start of
-	 *                    the server.
-	 */
-	private void startHttpServer(final Router router, final Future<Void> startFuture) {
-		final int httpPort = Parameter.HTTP_PORT.intValue(vertx, 8080);
-		final String defaultCertificateFile = this.getClass().getResource("/localhost.jks").getFile();
-		final String certificateFile = Parameter.TLS_KEYSTORE.stringValue(vertx, defaultCertificateFile);
+    /**
+     * Starts the HTTP server provided by this application. This server runs the
+     * Cyface collector REST-API.
+     * 
+     * @param router The router for all the endpoints the HTTP server should
+     *            serve.
+     * @param startFuture Informs the caller about the successful or failed start of
+     *            the server.
+     */
+    private void startHttpServer(final Router router, final Future<Void> startFuture) {
+        final int httpPort = Parameter.HTTP_PORT.intValue(vertx, 8080);
+        final String defaultCertificateFile = this.getClass().getResource("/localhost.jks").getFile();
+        final String certificateFile = Parameter.TLS_KEYSTORE.stringValue(vertx, defaultCertificateFile);
 
-		final HttpServerOptions options = new HttpServerOptions().setSsl(true)
-				.setKeyStoreOptions(new JksOptions().setPath(certificateFile).setPassword("secret"));
+        final HttpServerOptions options = new HttpServerOptions().setSsl(true).setKeyStoreOptions(new JksOptions()
+                .setPath(certificateFile).setPassword(Parameter.TLS_KEYSTORE_PASSWORD.stringValue(vertx, "secret")));
 
-		vertx.createHttpServer(options).requestHandler(router::accept).listen(httpPort,
-				serverStartup -> completeStartup(serverStartup, startFuture));
-	}
+        vertx.createHttpServer(options).requestHandler(router::accept).listen(httpPort,
+                serverStartup -> completeStartup(serverStartup, startFuture));
+    }
 
-	/**
-	 * Finishes the <code>MainVerticle</code> startup process and informs all
-	 * interested parties about whether it has been successful or not.
-	 * 
-	 * @param serverStartup
-	 * @param future
-	 */
-	private void completeStartup(final AsyncResult<HttpServer> serverStartup, final Future<Void> future) {
-		if (serverStartup.succeeded()) {
-			future.complete();
-		} else {
-			future.fail(serverStartup.cause());
-		}
-	}
+    /**
+     * Finishes the <code>MainVerticle</code> startup process and informs all
+     * interested parties about whether it has been successful or not.
+     * 
+     * @param serverStartup
+     * @param future
+     */
+    private void completeStartup(final AsyncResult<HttpServer> serverStartup, final Future<Void> future) {
+        if (serverStartup.succeeded()) {
+            future.complete();
+        } else {
+            future.fail(serverStartup.cause());
+        }
+    }
 
 }
--- src/main/java/de/cyface/collector/Parameter.java
+++ src/main/java/de/cyface/collector/Parameter.java
@@ -1,20 +1,16 @@
 /*
  * Copyright 2018 Cyface GmbH
- * 
  * This file is part of the Cyface Data Collector.
- *
- *  The Cyface Data Collector is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *  
- *  The Cyface Data Collector is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with the Cyface Data Collector.  If not, see <http://www.gnu.org/licenses/>.
+ * The Cyface Data Collector is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * The Cyface Data Collector is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with the Cyface Data Collector. If not, see <http://www.gnu.org/licenses/>.
  */
 package de.cyface.collector;
 
@@ -28,110 +24,118 @@ import io.vertx.core.logging.LoggerFactory;
  * to configure the application.
  * 
  * @author Klemens Muthmann
- * @version 1.0.0
+ * @version 1.1.0
  * @since 2.0.0
  */
 public enum Parameter {
-	/**
-	 * The location of the keystore used to sign JWT tokens.
-	 */
-	JWT_KEYSTORE("keystore.jwt"),
-	/**
-	 * The server port the API shall be available at.
-	 */
-	HTTP_PORT("http.port"),
-	/**
-	 * The location of the keystore for HTTPS connections.
-	 */
-	TLS_KEYSTORE("keystore.tls"),
-	/**
-	 * Detailed connection information about the Mongo user database. This database
-	 * stores all the credentials of users capable of logging in to the systems.
-	 * This should be a JSON object with supported parameters explained at: <a href=
-	 * "https://vertx.io/docs/vertx-mongo-client/java/#_configuring_the_client">https://vertx.io/docs/vertx-mongo-client/java/#_configuring_the_client</a>.
-	 */
-	MONGO_USER_DB("mongo.userdb"),
-	/**
-	 * Detailed connection information about the Mongo data database. This database
-	 * stores all data received via the REST-API. This should be a JSON object with
-	 * supported parameters explained at: <a href=
-	 * "https://vertx.io/docs/vertx-mongo-client/java/#_configuring_the_client">https://vertx.io/docs/vertx-mongo-client/java/#_configuring_the_client</a>.
-	 */
-	MONGO_DATA_DB("mongo.datadb");
+    /**
+     * The location of the keystore used to sign JWT tokens.
+     */
+    JWT_KEYSTORE("keystore.jwt"),
+    /**
+     * The parameter used to provide the keystore password for the JWT keystore.
+     */
+    JWT_KEYSTORE_PASSWORD("keystore.jwt.password"),
+    /**
+     * The server port the API shall be available at.
+     */
+    HTTP_PORT("http.port"),
+    /**
+     * The location of the keystore for HTTPS connections.
+     */
+    TLS_KEYSTORE("keystore.tls"),
+    /**
+     * The parameter used to provide the TLS keystore password.
+     */
+    TLS_KEYSTORE_PASSWORD("keystore.tls.password"),
+    /**
+     * Detailed connection information about the Mongo user database. This database
+     * stores all the credentials of users capable of logging in to the systems.
+     * This should be a JSON object with supported parameters explained at: <a href=
+     * "https://vertx.io/docs/vertx-mongo-client/java/#_configuring_the_client">https://vertx.io/docs/vertx-mongo-client/java/#_configuring_the_client</a>.
+     */
+    MONGO_USER_DB("mongo.userdb"),
+    /**
+     * Detailed connection information about the Mongo data database. This database
+     * stores all data received via the REST-API. This should be a JSON object with
+     * supported parameters explained at: <a href=
+     * "https://vertx.io/docs/vertx-mongo-client/java/#_configuring_the_client">https://vertx.io/docs/vertx-mongo-client/java/#_configuring_the_client</a>.
+     */
+    MONGO_DATA_DB("mongo.datadb");
 
-	/**
-	 * The logger used for objects of this class. You can change its configuration
-	 * by changing the values in
-	 * <code>src/main/resources/vertx-default-jul-logging.properties</code>.
-	 */
-	private final static Logger LOGGER = LoggerFactory.getLogger(Parameter.class);
+    /**
+     * The logger used for objects of this class. You can change its configuration
+     * by changing the values in
+     * <code>src/main/resources/vertx-default-jul-logging.properties</code>.
+     */
+    private final static Logger LOGGER = LoggerFactory.getLogger(Parameter.class);
 
-	/**
-	 * The parameter key used to load its value from the JSON configuration.
-	 */
-	private final String key;
+    /**
+     * The parameter key used to load its value from the JSON configuration.
+     */
+    private final String key;
 
-	/**
-	 * Creates a new completely initialized parameter.
-	 * 
-	 * @param key The parameter key used to load its value from the JSON
-	 *            configuration.
-	 */
-	private Parameter(final String key) {
-		this.key = key;
-	}
+    /**
+     * Creates a new completely initialized parameter.
+     * 
+     * @param key The parameter key used to load its value from the JSON
+     *            configuration.
+     */
+    private Parameter(final String key) {
+        this.key = key;
+    }
 
-	/**
-	 * @return The parameter key used to load its value from the JSON configuration.
-	 */
-	public String key() {
-		return key;
-	}
+    /**
+     * @return The parameter key used to load its value from the JSON configuration.
+     */
+    public String key() {
+        return key;
+    }
 
-	/**
-	 * Provides the string value of this parameter from the Vert.x configuration or
-	 * the <code>defaultValue</code> if there was none.
-	 * 
-	 * @param vertx        The <code>Vertx</code> instance containing the
-	 *                     configuration.
-	 * @param defaultValue A default value if the parameter was missing.
-	 * @return Either the value of the parameter as a <code>String</code> or the
-	 *         <code>defaultValue</code>.
-	 */
-	public String stringValue(final Vertx vertx, final String defaultValue) {
-		final String ret = vertx.getOrCreateContext().config().getString(key);
-		return ret == null ? defaultValue : ret;
-	}
+    /**
+     * Provides the string value of this parameter from the Vert.x configuration or
+     * the <code>defaultValue</code> if there was none.
+     * 
+     * @param vertx The <code>Vertx</code> instance containing the
+     *            configuration.
+     * @param defaultValue A default value if the parameter was missing.
+     * @return Either the value of the parameter as a <code>String</code> or the
+     *         <code>defaultValue</code>.
+     */
+    public String stringValue(final Vertx vertx, final String defaultValue) {
+        final String ret = vertx.getOrCreateContext().config().getString(key);
+        return ret == null ? defaultValue : ret;
+    }
 
-	/**
-	 * Provides the integer value of this parameter from the Vert.x configuration or
-	 * the <code>defaultValue</code> if there was none.
-	 * 
-	 * @param vertx        The <code>Vertx</code> instance containing the
-	 *                     configuration.
-	 * @param defaultValue A default value if the parameter was missing.
-	 * @return Either the value of the parameter as an <code>int</code> or the
-	 *         <code>defaultValue</code>.
-	 * @throws ClassCastException If the value was not an integer.
-	 */
-	public int intValue(final Vertx vertx, final int defaultValue) {
-		final Integer ret = vertx.getOrCreateContext().config().getInteger(key);
-		return ret == null ? defaultValue : ret;
-	}
+    /**
+     * Provides the integer value of this parameter from the Vert.x configuration or
+     * the <code>defaultValue</code> if there was none.
+     * 
+     * @param vertx The <code>Vertx</code> instance containing the
+     *            configuration.
+     * @param defaultValue A default value if the parameter was missing.
+     * @return Either the value of the parameter as an <code>int</code> or the
+     *         <code>defaultValue</code>.
+     * @throws ClassCastException If the value was not an integer.
+     */
+    public int intValue(final Vertx vertx, final int defaultValue) {
+        final Integer ret = vertx.getOrCreateContext().config().getInteger(key);
+        return ret == null ? defaultValue : ret;
+    }
 
-	/**
-	 * Provides the JSON value of this parameter from the Vert.x configuration or
-	 * the <code>defaultValue</code> if there was none.
-	 * 
-	 * @param vertx        The <code>Vertx</code> instance containing the
-	 *                     configuration.
-	 * @param defaultValue A default value if the parameter was missing.
-	 * @return Either the value of the parameter as a JSON object or the
-	 *         <code>defaultValue</code>.
-	 */
-	public JsonObject jsonValue(final Vertx vertx, JsonObject defaultValue) {
-		final JsonObject ret = vertx.getOrCreateContext().config().getJsonObject(key);
-		LOGGER.info("Read json value " + ret + " for key " + key);
-		return ret == null ? defaultValue : ret;
-	}
+    /**
+     * Provides the JSON value of this parameter from the Vert.x configuration or
+     * the <code>defaultValue</code> if there was none.
+     * 
+     * @param vertx The <code>Vertx</code> instance containing the
+     *            configuration.
+     * @param defaultValue A default value if the parameter was missing.
+     * @return Either the value of the parameter as a JSON object or the
+     *         <code>defaultValue</code>.
+     */
+    public JsonObject jsonValue(final Vertx vertx, JsonObject defaultValue) {
+        final JsonObject ret = vertx.getOrCreateContext().config().getJsonObject(key);
+        LOGGER.info("Read json value " + ret + " for key " + key);
+        return ret == null ? defaultValue : ret;
+    }
 }
