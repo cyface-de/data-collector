a0cea6d75b9fead2b4846b4abc3c566f0ce65cbf
Klemens Muthmann <klemens.muthmann@cyface.de>
2020-12-28 09:06:46 +0100

    Add assertions to AuthorisationTest

--- src/test/java/de/cyface/collector/AuthorisationTest.java
+++ src/test/java/de/cyface/collector/AuthorisationTest.java
@@ -1,5 +1,9 @@
 package de.cyface.collector;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
@@ -37,14 +41,18 @@ public class AuthorisationTest {
 
         options.addPubSecKey(keyOptions)
                 .setJWTOptions(new JWTOptions().setIssuer(issuer).setAudience(Collections.singletonList(issuer)));
-        final var authoriser = JWTAuth.create(vertx, options);
+        final var authorizer = JWTAuth.create(vertx, options);
         final var body = new JsonObject().put("username", "test").put("password", "test-password").put("aud", issuer)
                 .put("iss", issuer);
-        final var token = authoriser.generateToken(body);
-        System.out.println(token);
-        final var authenticationFuture = authoriser.authenticate(new TokenCredentials(token));
+        final var token = authorizer.generateToken(body);
+        final var authenticationFuture = authorizer.authenticate(new TokenCredentials(token));
         authenticationFuture.onComplete(ctx.succeeding(user -> {
-            System.out.println(user);
+            ctx.verify(() -> {
+                assertThat(user.principal().getString("username"), is(equalTo("test")));
+                assertThat(user.principal().getString("password"), is(equalTo("test-password")));
+                assertThat(user.principal().getString("aud"), is(equalTo(issuer)));
+                assertThat(user.principal().getString("iss"), is(equalTo(issuer)));
+            });
             ctx.completeNow();
         }));
     }
