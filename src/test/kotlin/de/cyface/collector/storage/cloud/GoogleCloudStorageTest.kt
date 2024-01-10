7aa8380cc46355003271a74a70f4daaa461dc37a
Klemens Muthmann <klemens.muthmann@cyface.de>
2024-01-10 12:04:57 +0100

    Revert ignoring GoogleCloudStorageTest

--- src/test/kotlin/de/cyface/collector/storage/cloud/GoogleCloudStorageTest.kt
+++ src/test/kotlin/de/cyface/collector/storage/cloud/GoogleCloudStorageTest.kt
@@ -44,7 +44,6 @@ import java.util.concurrent.CountDownLatch
 import java.util.concurrent.Flow
 import java.util.concurrent.SubmissionPublisher
 import java.util.concurrent.TimeUnit
-import kotlin.test.Ignore
 import kotlin.test.Test
 
 /**
@@ -54,7 +53,6 @@ import kotlin.test.Test
  * @author Klemens Muthmann
  * @version 1.0.1
  */
-@Ignore
 class GoogleCloudStorageTest {
 
     /**
