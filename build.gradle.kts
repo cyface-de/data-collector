3d7bbf103360d79adca2abf4a60959a605b9a09b
Klemens Muthmann <klemens.muthmann@cyface.de>
2024-04-30 08:12:54 +0200

    Update Vert.x Version

--- build.gradle.kts
+++ build.gradle.kts
@@ -87,7 +87,7 @@ kotlin {
   jvmToolchain(17)
 }
 
-val vertxVersion = "4.5.0"
+val vertxVersion = "4.5.7"
 val micrometerVersion = "1.10.6"
 val commonsLangVersion = "3.12.0"
 val logbackVersion = "1.4.6"
